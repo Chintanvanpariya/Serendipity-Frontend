264cf0b8a900f9b4f1c3fe908eb022ad3a3a3ae2
chintan5588@gmail.com <chintan5588@gmail.com>
2021-01-26 16:56:38 -0600

    adding token interceptor

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -1,14 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
-
-const httpOptions = {
-    headers: new HttpHeaders({
-      Authorization:'Bearer '+ JSON.parse(localStorage.getItem('user'))?.token
-    })
-}
 
 @Injectable({
   providedIn: 'root'
@@ -22,10 +15,10 @@ export class MembersService {
   baseUrl = environment.apiUrl;
  
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl+'users', httpOptions) ;
+    return this.http.get<Member[]>(this.baseUrl+'users') ;
   }
 
   getMember(username:string){
-    return this.http.get<Member>(this.baseUrl+'users/' + username, httpOptions) ;
+    return this.http.get<Member>(this.baseUrl+'users/' + username) ;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -46,6 +47,11 @@ import { MemberCardComponent } from './members/member-card/member-card.component
     {
       provide:HTTP_INTERCEPTORS,
       useClass:ErrorInterceptor,
+      multi :true
+    },
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:JwtInterceptor,
       multi :true
     }
   ],
