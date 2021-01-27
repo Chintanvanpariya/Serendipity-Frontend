83b2ebdba4f860bc23859f30b8bc5de508840397
chintan5588@gmail.com <chintan5588@gmail.com>
2021-01-27 00:31:21 -0600

    added edit form changes

---
+++ src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
+  canDeactivate(
+    component: MemberEditComponent):boolean {
+      if(component.editForm.dirty){
+        return confirm('Are you sure you want to continue ?, unsaved changes would be lost')
+      }
+    return true;
+  }
+
+
+  
+}
