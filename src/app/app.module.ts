8eda4addb9df85e9c63a8a8979983462ca9d5ae3
chintan5588@gmail.com <chintan5588@gmail.com>
2021-01-31 16:02:25 -0600

    make reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
@@ -51,6 +51,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     FormsModule,
     SharedModule,
     NgxSpinnerModule,
+    ReactiveFormsModule
   ],
   providers: [
     {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi :true},
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,Input, Output,EventEmitter } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -11,6 +12,7 @@ export class RegisterComponent implements OnInit {
 
   model:any={};
   @Output() cancelRegister=new EventEmitter();
+  registerForm:FormGroup;
 
   constructor(
     private accountService:AccountService,
@@ -18,19 +20,37 @@ export class RegisterComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.initializeform();
   }
 
+  initializeform(){
+    this.registerForm= new FormGroup({
+      username:new FormControl('',Validators.required),
+      password:new FormControl('',[Validators.minLength(4),
+      Validators.maxLength(12),Validators.required ]),
+      confirmPassword:new FormControl('',[Validators.required,this.matchValue('password')]),
+    })
+  }
+
+  matchValue(matchTo:string):ValidatorFn {
+    return (control:AbstractControl)=>{
+      return control?.value === control?.parent.controls[matchTo].value 
+      ? null : {isMatching : true}
+    }
+  } 
+
   register(){
-    console.log(this.model);
-    this.accountService.register(this.model).subscribe(
-      response => {
-        console.log(response);
-        this.cancel();
-      }
-    ,error => {
-     console.log(error);
-     this.toast.error(error.error);
-    });
+     console.log(this.registerForm.value);
+    // console.log(this.model);
+    // this.accountService.register(this.model).subscribe(
+    //   response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   }
+    // ,error => {
+    //  console.log(error);
+    //  this.toast.error(error.error);
+    // });
   }
 
   cancel(){
