cb9c15745148f3829abd2beef6eeae15e98aa566
chintan5588@gmail.com <chintan5588@gmail.com>
2021-02-05 15:09:05 -0600

    pagination client side added

--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Member } from 'src/app/models/member';
+import { Pagination } from 'src/app/models/pagination';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -10,14 +11,30 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberListComponent implements OnInit {
 
-  members$:Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  pageNumber: 1;
+  pageSize = 5;
 
   constructor(
-    private memberService:MembersService
+    private memberService: MembersService
   ) { }
 
   ngOnInit(): void {
-      this.members$= this.memberService.getMembers();
+    this.loadMembers();
   }
 
+  loadMembers() {
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe(reponse => {
+      this.members = reponse.result;
+      this.pagination = reponse.pagination;
+    })
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.page;
+    this.loadMembers();
+  }
+
+  
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import { PaginationModule} from 'ngx-bootstrap/pagination';
 import { BsDatepickerModule} from 'ngx-bootstrap/datepicker/';
 import { TabsModule} from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
@@ -15,11 +16,12 @@ import { FileUploadModule } from 'ng2-file-upload';
     BsDropdownModule.forRoot(),
     BsDatepickerModule.forRoot(),
     TabsModule.forRoot(),
-    NgxGalleryModule,
-    FileUploadModule,
+    PaginationModule.forRoot(),
     ToastrModule.forRoot({
       positionClass:'toast-bottom-right'
-    })
+    }),
+    NgxGalleryModule,
+    FileUploadModule,
   ],
   exports:[
     BsDropdownModule,
@@ -28,6 +30,7 @@ import { FileUploadModule } from 'ng2-file-upload';
     NgxGalleryModule,
     FileUploadModule,
     BsDatepickerModule,
+    PaginationModule,
   ]
 })
 export class SharedModule { }
